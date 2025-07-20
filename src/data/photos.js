1ac0429da26a889cf17a345baa9777c43bc43849
MokyNZ <mokynzmc@gmail.com>
2025-07-21 04:16:49 +1200

    Initial commit

---
+++ src/data/photos.js
@@ -0,0 +1,15 @@
+// src/data/photos.js
+export default [
+  {
+    slug: 'my-first-picture',
+    title: 'My first picture',
+    image: '/images/first_ever_picture.png',
+    description: 'The first astro picture I took with a DSLR. The milky way (with clouds in the way...)',
+    camera: 'Canon EOS 1000D',
+    lens: 'Canon 18-55mm f/3.5-5 IS',
+    exposure: '66x10s (11 minutes)',
+    iso: 'ISO 1600',
+    aperture: 'f/3.5',
+    date: '2025-07-18'
+  },
+];
