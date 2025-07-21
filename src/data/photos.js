faf2473915ac83a9bed3d64f2bdc4fd9d7e7e0eb
MokyNZ <mokynzmc@gmail.com>
2025-07-21 22:05:11 +1200

    Added a new photo to the portfolio

--- src/data/photos.js
+++ src/data/photos.js
@@ -6,10 +6,22 @@ export default [
     image: '/images/first_ever_picture.png',
     description: 'The first astro picture I took with a DSLR. The milky way (with clouds in the way...)',
     camera: 'Canon EOS 1000D',
-    lens: 'Canon 18mm f/3.5-5 IS',
+    lens: 'Canon 18mm f/3.5 IS',
     exposure: '66x10s (11 minutes)',
     iso: 'ISO 1600',
     aperture: 'f/3.5',
     date: '2025-07-18'
+  },
+  {
+    slug: 'milky_way_actually_good',
+    title: 'theres a milk in the way',
+    image: '/images/milky_way_actually_good/jpg',
+    description: 'I figured out how to edit properly :)',
+    camera: 'Canon EOS 1000D',
+    lens: 'Canon 18mm f/3.5 IS',
+    exposure: '139x10s (23.16 minutes)',
+    iso: 'ISO 1600',
+    aperture: 'f/3.5',
+    date: '2025-07-21'
   },
 ];
