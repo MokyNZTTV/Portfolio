f342412e53f2240e2bcbfb7ea254086065dd9dc6
MokyNZTTV <mokynzmc@gmail.com>
2025-11-13 15:03:32 +1300

    added focal length in shot info

--- src/data/photos.js
+++ src/data/photos.js
@@ -6,10 +6,11 @@ export default [
     image: '/images/first_ever_picture.png',
     description: 'The first astro picture I took with a DSLR. The milky way (with clouds in the way...)',
     camera: 'Canon EOS 1000D',
-    lens: 'Canon 18mm f/3.5 IS',
+    lens: 'Canon 18-55mm f/3.5-5.6 IS',
     exposure: '66x10s (11 minutes)',
-    iso: 'ISO 1600',
+    focal_length: '18mm',
     aperture: 'f/3.5',
+    iso: 'ISO 1600',
     date: '2025-07-18'
   },
   {
@@ -18,10 +19,11 @@ export default [
     image: '/images/milky_way_actually_decent.jpg',
     description: 'I figured out how to edit properly :)',
     camera: 'Canon EOS 1000D',
-    lens: 'Canon 18mm f/3.5 IS',
+    lens: 'Canon 18-55mm f/3.5-5.6 IS',
     exposure: '139x10s (23.16 minutes)',
-    iso: 'ISO 1600',
+    focal_length: '18mm',
     aperture: 'f/3.5',
+    iso: 'ISO 1600',
     date: '2025-07-21'
   },
   {
@@ -30,10 +32,11 @@ export default [
     image: '/images/first_orion_picture.jpg',
     description: 'I just wanted to get a picture of orion, even though it was a full moon.<br>I don\'t know if I like it tbh',
     camera: 'Canon EOS 1000D',
-    lens: 'Canon EF 100mm f/5.6',
+    lens: 'Canon EF 100-300mm f/4.5-5.6',
+    focal_length: '100mm',
     exposure: '85x4s (5.66 minutes)',
-    iso: 'ISO 400',
     aperture: 'f/5.6',
+    iso: 'ISO 400',
     date: '2025-11-07'
   },
 ];
